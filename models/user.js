704f803ef86ee4f40c0c4de5854d8385031f004c
Alexander Moiseyev <daklaktakpak@yandex.ru>
2020-08-22 11:25:18 +0500

    add email & password to userSchema

--- models/user.js
+++ models/user.js
@@ -23,6 +23,30 @@ const userSchema = new mongoose.Schema({
       },
     },
   },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(email) {
+        return validator.isEmail(email);
+      },
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    validate: {
+      validatePassword(string) {
+        const password = /^[A-Za-z]\w{7,14}$/;
+        if (string.match(password)) {
+          return true;
+        }
+        return false;
+      },
+    },
+  },
 });
 
 module.exports = mongoose.model('user', userSchema);
